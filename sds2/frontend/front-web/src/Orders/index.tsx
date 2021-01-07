4091738648b4954ea61c3084301abc50d530c719
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-07 17:34:26 -0300

    parte 41

--- sds2/frontend/front-web/src/Orders/index.tsx
+++ sds2/frontend/front-web/src/Orders/index.tsx
@@ -1,8 +1,21 @@
+import { useState, useEffect } from 'react'
 import './styles.css'
 import StepsHeader from './StepsHeader'
 import ProductsList from './ProductsList'
+import { Product } from './types'
+import { fetchProducts } from '../api'
 
 export default function Orders() {
+    const [products, setProducts] = useState<Product[]>([])
+
+    console.log(products)
+
+    useEffect(() => {
+       fetchProducts()
+         .then(response => setProducts(response.data))
+         .catch(error => console.log(error))
+    }, [])
+
     return (
         <div className="orders-container">
             <StepsHeader />
