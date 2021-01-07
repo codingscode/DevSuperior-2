5cad2df9aa020caf8f9040223c950f122d4242f2
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-07 16:32:58 -0300

    parte 38

---
+++ sds2/frontend/front-web/src/Orders/ProductsList.tsx
@@ -0,0 +1,11 @@
+
+
+export default function ProductsList() {
+    return (
+        <div className="orders-list-container">
+            <div className="orders-list-content">
+                products list
+            </div>
+        </div>
+    )
+}
