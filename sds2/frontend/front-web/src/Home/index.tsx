27934bc3776977fa9d834967316c1925fc8fbc26
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-07 12:28:25 -0300

    parte 28

--- sds2/frontend/front-web/src/Home/index.tsx
+++ sds2/frontend/front-web/src/Home/index.tsx
@@ -1,12 +1,19 @@
 import './styles.css'
-
+import { ReactComponent as MainImage } from './main.svg'
 
 
 export default function Home() {
     return (
         <div className="home-container">
              <div className="home-content">
-                 <h1>Home content</h1>
+                 <div className="home-actions">
+                      <h1 className="home-title">Faça seu pedido<br/> que entregamos<br/> pra você!!!</h1>
+                      <h3 className="home-subtitle">Escolha o seu pedido e em poucos minutos<br/>levaremos na sua porta</h3>
+                      <a href="orders" className="home-btn-order">FAZER PEDIDO</a>
+                 </div>
+                 <div className="home-image">
+                      <MainImage />
+                 </div>
              </div>
         </div>
     )
