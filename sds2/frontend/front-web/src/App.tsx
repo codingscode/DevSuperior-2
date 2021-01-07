3a9bd57c91e24f4462789d4814ace2db18abab7e
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-07 12:12:47 -0300

    parte 27

--- sds2/frontend/front-web/src/App.tsx
+++ sds2/frontend/front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './Navbar'
+import Home from './Home'
 
 
 export default function App() {
   return (
-    <div>
+    <>
         <Navbar />
-    </div>
+        <Home />
+    </>
   )
 }
---
+++ sds2/frontend/front-web/src/Home/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+
+
+export default function Home() {
+    return (
+        <div className="home-container">
+             <div className="home-content">
+                 <h1>Home content</h1>
+             </div>
+        </div>
+    )
+}
