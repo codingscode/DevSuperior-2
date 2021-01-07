d0756e0d9c32a37f5f69464dfa2820119bfd884f
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-07 10:55:18 -0300

    parte 24

--- sds2/frontend/front-web/src/App.tsx
+++ sds2/frontend/front-web/src/App.tsx
@@ -1,15 +1,12 @@
 import './App.css'
-import Hello from './Hello'
+import Counter from './Counter'
 
 
 export default function App() {
   return (
     <div>
-        <h1>Hello SDS2</h1>
-        <Hello message="Dev" />
-        <Hello message="Programador" />
-        <Hello message="Cientista" />
-        <Hello message="Estudante" />
+        
+        <Counter />
     </div>
   )
 }
