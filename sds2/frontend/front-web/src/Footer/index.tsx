783840f4a0b1b4866808e4fab7202dcccfe56b22
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-07 12:45:35 -0300

    parte 30

---
+++ sds2/frontend/front-web/src/Footer/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+
+
+export default function Footer() {
+    return (
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+        </footer>
+    )
+}
