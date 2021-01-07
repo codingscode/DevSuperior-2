46bc86bbffe37d2d8e9ce66508c5a7701f3df6d4
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-07 16:51:13 -0300

    parte 40

--- sds2/frontend/front-web/src/Orders/ProductsList.tsx
+++ sds2/frontend/front-web/src/Orders/ProductsList.tsx
@@ -3,7 +3,15 @@ import ProductCard from './ProductCard'
 export default function ProductsList() {
     return (
         <div className="orders-list-container">
-            <div className="orders-list-content">
+            <div className="orders-list-items">
+               <ProductCard />
+               <ProductCard />
+               <ProductCard />
+               <ProductCard />
+               <ProductCard />
+               <ProductCard />
+               <ProductCard />
+               <ProductCard />
                <ProductCard />
             </div>
         </div>
