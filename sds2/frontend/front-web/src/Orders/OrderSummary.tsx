04f32734ab09b86109e0cc7bf672bac6c276a7dc
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-08 18:04:07 -0300

    parte 56

--- sds2/frontend/front-web/src/Orders/OrderSummary.tsx
+++ sds2/frontend/front-web/src/Orders/OrderSummary.tsx
@@ -3,12 +3,15 @@ export default function OrderSummary() {
      return (
          <div className="order-summary-container">
              <div className="order-summary-content">
-                 <span className="amount-selected-container">
-                     <strong>2</strong>PEDIDOS SELECIONADOS
-                 </span>
-                 <span className="order-summary-total">
-                     <strong>R$ 50,00</strong>VALOR TOTAL
-                 </span>
+                <div>
+                    <span className="amount-selected-container">
+                        <strong className="amount-selected">2</strong>PEDIDOS SELECIONADOS
+                    </span>
+                    <span className="order-summary-total">
+                        <strong className="amount-selected">R$ 50,00</strong>VALOR TOTAL
+                    </span>
+                </div>
+                <button className="order-summary-make-order" >FAZER PEDIDO</button>
              </div>
          </div>
      )
