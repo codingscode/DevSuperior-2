1c797bfbf2bccb4cd3afdec2fb8cba882835d483
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-08 15:30:14 -0300

    parte 50

--- sds2/frontend/front-web/src/Orders/OrderLocation.tsx
+++ sds2/frontend/front-web/src/Orders/OrderLocation.tsx
@@ -1,7 +1,8 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
+
 const position = {
-    lat: 51.505, lng: -0.09
+    lat: -2.548681, lng: -44.174140
 }
 
 export default function OrderLocation() {
@@ -12,14 +13,14 @@ export default function OrderLocation() {
             <div className="order-location-content">
                 <h3 className="order-location-title">Selecione onde o pedido deve ser entregue:</h3>
                 <div className="filter-container"></div>
-                <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                <MapContainer center={position} zoom={15.5} scrollWheelZoom={true}>
                     <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
                     <Marker position={position}>
                         <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
+                            Minha localização. <br /> Easily customizable.
                         </Popup>
                     </Marker>
                 </MapContainer>
