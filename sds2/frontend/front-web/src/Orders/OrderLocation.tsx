cbc054a0eee0b43b7a353bdabe794e1b6d776550
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-08 16:27:33 -0300

    parte 51

--- sds2/frontend/front-web/src/Orders/OrderLocation.tsx
+++ sds2/frontend/front-web/src/Orders/OrderLocation.tsx
@@ -1,24 +1,69 @@
+import { useState } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import AsyncSelect from 'react-select/async'
+import { fetchLocalMapBox } from '../api'
 
 
-const position = {
+const initialPosition = {
     lat: -2.548681, lng: -44.174140
 }
 
+type Place = {
+    label?: string   // ?: -> significa n obrigatório
+    value?: string
+    position: {
+        lat: number
+        lng: number
+    }
+}
+
 export default function OrderLocation() {
+    const [address, setAddress] = useState<Place>({
+        position: initialPosition
+    })
     
+    const loadOptions = async (inputValue: string, callback: (places: Place[]) => void) => {
+        const response = await fetchLocalMapBox(inputValue)
+      
+        const places = response.data.features.map((item: any) => {
+          return ({
+            label: item.place_name,
+            value: item.place_name,
+            position: {
+              lat: item.center[1],
+              lng: item.center[0]
+            },
+            place: item.place_name,
+          })
+        })
+      
+        callback(places)
+    }
+      
+    const handleChangeSelect = (place: Place) => {
+        setAddress(place)
+        /* onChangeLocation({
+            latitude: place.position.lat,
+            longitude: place.position.lng,
+            address: place.label!
+        }) */
+    }
     
     return (
         <div className="order-location-container">
             <div className="order-location-content">
                 <h3 className="order-location-title">Selecione onde o pedido deve ser entregue:</h3>
-                <div className="filter-container"></div>
-                <MapContainer center={position} zoom={15.5} scrollWheelZoom={true}>
+                <div className="filter-container">
+                    <AsyncSelect placeholder="Digite um endereço para entregar o pedido"
+                           className="filter" loadOptions={loadOptions} onChange={value => handleChangeSelect(value as Place)} />
+
+                </div>
+                <MapContainer center={address.position} zoom={15.5} scrollWheelZoom={true}>
                     <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={position}>
+                    <Marker position={address.position}>
                         <Popup>
                             Minha localização. <br /> Easily customizable.
                         </Popup>
