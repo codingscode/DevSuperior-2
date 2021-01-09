c1f901dbbeba745603e8cf70944f9c219d01e849
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-09 17:23:30 -0300

    parte 67

---
+++ sds2/mobile/front-mobile/src/Header/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Header() {
+  return (
+    <View style={styles.container}>
+      <Text>DS Delivery</Text>
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+      backgroundColor: '#DA5C5C'
+  },
+})
