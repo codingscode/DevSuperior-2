c0fb7bb023ae535cf08e7dda0a2a5a781375840a
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-10 17:39:35 -0300

    parte 80

--- sds2/mobile/front-mobile/src/Orders/index.tsx
+++ sds2/mobile/front-mobile/src/Orders/index.tsx
@@ -1,31 +1,31 @@
-import React, { useEffect } from 'react'
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, ScrollView, Alert } from 'react-native'
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
 import { fetchOrders } from '../api'
 import Header from '../Header'
 import OrderCard from '../OrderCard'
+import { Order } from '../types'
 
 
 
 export default function Orders() {
+    const [orders, setOrders] = useState<Order[]>([])
 
     useEffect(() => {
         fetchOrders()
-           .then(response => console.log(response))
-           .catch(error => console.log(error))
+           .then(response => setOrders(response.data))
+           .catch(() => Alert.alert('Houve um erro ao buscar os pedidos!'))
     }, [])
   
     return (
         <>
             <Header />
             <ScrollView style={styles.container} >
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
+                {orders.map(order => (
+                    <TouchableWithoutFeedback key={order.id}>
+                        <OrderCard order={order} />
+                    </TouchableWithoutFeedback>
+                ))}
             </ScrollView>
         </>
     )
