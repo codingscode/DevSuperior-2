7321bd5fb812fc291e8252c5dc54dff8e60dc128
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-10 16:26:40 -0300

    parte 78

--- sds2/mobile/front-mobile/src/api.ts
+++ sds2/mobile/front-mobile/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8080'
+const API_URL = 'http://192.168.1.8:8080'
 
 export function fetchOrders() {
     return axios(`${API_URL}/orders`)
