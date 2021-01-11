773e7d40afc6c592ffdf6afc7fea916000798be6
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-11 12:49:07 -0300

    parte 88 finalização mobile

--- sds2/mobile/front-mobile/src/OrderDetails/index.tsx
+++ sds2/mobile/front-mobile/src/OrderDetails/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image, Alert } from 'react-native'
+import { StyleSheet, Text, View, Image, Alert, Linking } from 'react-native'
 
 import { useNavigation } from '@react-navigation/native'
 import Header from '../Header'
@@ -35,12 +35,16 @@ export default function OrderDetails({ route }: Props) {
         })
     }
 
+    const handleStartNavigation = () => {
+       Linking.openURL(`https://www.google.com/maps/dir/?api=1&travelmode=driving&dir_action=navigate&destination=${order.latitude},${order.longitude}`)
+    }
+
     return (
         <>
            <Header />
            <View style={styles.container}>
               <OrderCard order={order} />
-              <RectButton style={styles.button}>
+              <RectButton style={styles.button} onPress={handleStartNavigation} >
                  <Text style={styles.buttonText}>INICIAR NAVEGAÇÃO</Text>
               </RectButton>
               <RectButton style={styles.button} onPress={handleConfirmDelivery} >
