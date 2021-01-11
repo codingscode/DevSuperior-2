43b58c3d40ed87da6ca0f8619244d38ac5de50c1
Heronnes Pereira <heronnestg@hotmail.com>
2021-01-11 11:50:14 -0300

    parte 85

--- sds2/mobile/front-mobile/src/OrderDetails/index.tsx
+++ sds2/mobile/front-mobile/src/OrderDetails/index.tsx
@@ -4,6 +4,8 @@ import { StyleSheet, Text, View, Image } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import Header from '../Header'
 import { Order } from '../types'
+import OrderCard from '../OrderCard'
+import { RectButton } from 'react-native-gesture-handler'
 
 type Props = {
     route: {
@@ -24,13 +26,44 @@ export default function OrderDetails({ route }: Props) {
     return (
         <>
            <Header />
-           <View>
-              <Text>Detalhes do pedido {order.id}</Text>
+           <View style={styles.container}>
+              <OrderCard order={order} />
+              <RectButton style={styles.button}>
+                 <Text style={styles.buttonText}>INICIAR NAVEGAÇÃO</Text>
+              </RectButton>
+              <RectButton style={styles.button}>
+                 <Text style={styles.buttonText}>CONFIRMAR ENTREGA</Text>
+              </RectButton>
+              <RectButton style={styles.button}>
+                 <Text style={styles.buttonText}>CANCELAR</Text>
+              </RectButton>
            </View>
         </>
     )
 }
 
 const styles = StyleSheet.create({
-    
+    container: {
+        paddingRight: '5%',
+        paddingLeft: '5%'
+    },
+      button: {
+        backgroundColor: '#DA5C5C',
+        flexDirection: 'row',
+        borderRadius: 10,
+        marginTop: 40,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        paddingTop: 15,
+        paddingBottom: 15,
+        paddingLeft: 50,
+        paddingRight: 50,
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: '#FFF',
+        letterSpacing: -0.24,
+        fontFamily: 'OpenSans_700Bold'
+    }
 })
